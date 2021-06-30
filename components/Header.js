e54c8430adb766fa7526bb6d5efdc19d675a4ceb
Henry Doce <henrydoce@protonmail.com>
2021-06-29 22:54:48 -0500

    Can add and remove todos

---
+++ components/Header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const Header = () => {
+    return (
+        <View style={styles.header}>
+            <Text style={styles.title}>My Todos</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    header: {
+        height: 80,
+        paddingTop: 38,
+        backgroundColor: 'coral'
+    }, 
+    title: {
+        // textAlign: 'center',
+        alignSelf: 'center',
+        color: '#fff',
+        fontSize: 20,
+        fontWeight: 'bold', 
+        marginTop: 5
+    }
+});
+
+export default Header;
